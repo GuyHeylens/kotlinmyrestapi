553f327148db8b2fed95dd150117770af571bbda
guy@mosp.be <guy@mosp.be>
2017-11-01 13:19:17 +0100

    Added Swagger!

--- src/main/kotlin/be/kotlin/myrestapi/kotlinmyrestapi/KotlinmyrestapiApplication.kt
+++ src/main/kotlin/be/kotlin/myrestapi/kotlinmyrestapi/KotlinmyrestapiApplication.kt
@@ -15,6 +15,11 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @SpringBootApplication
@@ -52,14 +57,7 @@ class KotlinmyrestapiApplication{
         repoBeer.save(BeerEntity(2,"Westvleteren Acht",  8.0, "Brown", 5, 1))
         repoBeer.save(BeerEntity(3,"Westvleteren Twaalf", 10.8, "Brown", 5, 1))
 
-
-
-
-
-
     }
-
-
 }
 
 
